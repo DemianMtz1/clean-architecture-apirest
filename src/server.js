fe5f468734da8c867247bcd4379fd107758f81d1
demian <ericdemian.martinez@gmail.com>
2021-05-21 21:35:55 -0500

    Modified server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,20 @@
 // este archivo aloja la definicion de nuestro servidor
+const { json } = require('express');
 const express = require('express');
 const kodersRouter = require('./routers/koders');
 const mentorsRouters = require('./routers/mentors')
 const app = express();
 
 app.use(express.json())
+app.use((req,res,next)=> {
+    let options = {
+        method: req.method,
+        path: req.url,
+        data: req.body
+    }
+    console.log(options)
+    next()
+})
 app.use('/koders', kodersRouter);
 app.use('/mentors', mentorsRouters);
 
